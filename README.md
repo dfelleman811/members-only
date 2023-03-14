0d67d8e0132ce730f29f818ebde98c53deea14a7
Daniel Felleman <daniel.felleman@revature.com>
2023-03-14 17:41:54 -0400

    adding simple tests

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 ## User Stories
 - Any user can view posts
 - Only Members can see who is the author
+
+
+## Startup and Versioning
+> You'll need the following software and tools
+
+### Ruby
+- Check to see if Ruby is installed by running `ruby -v` in your terminal. 
+- Follow [the documentation's installation guide](https://www.ruby-lang.org/en/documentation/installation/) to install Ruby if you need to.
+- This app was created with version ruby-3.2.0
+
+### Rails
+- Check to see if Rails is installed by running `rails -v` in your terminal.
+- Refer to the [Ruby on Rails guide](https://guides.rubyonrails.org/v5.0.1/getting_started.html) for how to install Rails if you need to.
+- This app was created using Rails 7.0.4.2
+
+### 
--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "member with required fields should be valid" do
+    @member = Member.new(email: "email@nothing.com", password: "password")
+    assert @member.valid?
+  end
+
+  test "member without password is invalid" do 
+    @member = Member.new(email: "test@email.com", name: "testuser")
+    assert_not @member.valid?
+  end
+
+  test "member without email is invalid" do 
+    @member = Member.new(name: "carrie", password: "passpass")
+    assert_not @member.valid?
+  end
+
+  test "member without email or password is invalid" do 
+    @member = Member.new(name: "johnny")
+    assert_not @member.valid? 
+  end
+
+  test "password must be at least 6 characters" do
+    @member = Member.new(email: "jd@email.com", name: "john doe", password: "pass")
+    assert_not @member.valid?
+  end
 end
